f72fc29d37adac7b4533a4d9e78631ca9c7ebd3e
taslav8 <taslavbauman@gmail.ru>
2023-07-08 13:14:45 +0300

    Second task

---
+++ main2/main2.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func calculate(arr []int) {
+	var wg sync.WaitGroup
+
+	for _, n := range arr {
+		wg.Add(1)
+
+		go func(n int) {
+			fmt.Println(n * n) //Расчет квадрата и вывод его в отдельной горутине
+			wg.Done()
+		}(n)
+	}
+
+	wg.Wait()
+
+}
+
+func main() {
+	arr := []int{2, 4, 6, 8, 10}
+
+	calculate(arr)
+}
+
+//Вывод ответов будет в случайном порядке
